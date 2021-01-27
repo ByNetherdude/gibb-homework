a25e4287d689f8225bc008ab47f266ed8be298f3
Janis Christen <janis.christen@sunrise.ch>
2021-01-27 15:59:13 +0100

    [TASK] Add ImageUpload

--- app/src/main/java/ch/bbcag/gibb_homework/CreateEditActivity.java
+++ app/src/main/java/ch/bbcag/gibb_homework/CreateEditActivity.java
@@ -1,22 +1,41 @@
 package ch.bbcag.gibb_homework;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
 
 import ch.bbcag.gibb_homework.constants.IntentContext;
+import ch.bbcag.gibb_homework.model.Task;
 
 public class CreateEditActivity extends AppCompatActivity {
 
     private String context;
     private static final int PICK_IMAGE = 1;
+    ImageView imageView;
+    Button btnOpen;
+    Task newTask = new Task();
+    Uri saveImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +51,30 @@ public class CreateEditActivity extends AppCompatActivity {
         } else {
             Log.e("Intent", "Invalid call!");
         }
+
+        imageView = findViewById(R.id.upload_image);
+        btnOpen = findViewById(R.id.button_upload);
+
+        if(ContextCompat.checkSelfPermission(CreateEditActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(CreateEditActivity.this, new String[]{Manifest.permission.CAMERA}, 100);
+        }
+
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "New Picture");
+                values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+                Uri imageUri = getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                saveImageUri = imageUri;
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, 100);
+                //startActivityForResult(intent, 100);
+            }
+        });
     }
 
     @Override
@@ -59,5 +102,31 @@ public class CreateEditActivity extends AppCompatActivity {
     public void uploadImage(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100) {
+                if (resultCode == Activity.RESULT_OK) {
+                    try {
+                        Bitmap thumbnail = MediaStore.Images.Media.getBitmap(
+                                getContentResolver(), saveImageUri);
+                        imageView.setImageBitmap(thumbnail);
+                        String imageurl = getRealPathFromURI(saveImageUri);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 }
