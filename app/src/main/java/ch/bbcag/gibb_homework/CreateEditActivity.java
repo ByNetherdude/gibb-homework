8bd6e3bcffa71eec69ae87db3ea44c2005e7b19e
Janis Christen <janis.christen@sunrise.ch>
2021-01-27 09:10:10 +0100

    [TASK] Add UP-Button

--- app/src/main/java/ch/bbcag/gibb_homework/CreateEditActivity.java
+++ app/src/main/java/ch/bbcag/gibb_homework/CreateEditActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import ch.bbcag.gibb_homework.enums.IntentContext;
@@ -22,6 +23,7 @@ public class CreateEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_edit);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Back-Button
         createFABPlus();
 
         if(getIntent().getStringExtra(IntentContext.NAME) != null) {
@@ -29,6 +31,18 @@ public class CreateEditActivity extends AppCompatActivity {
             setActitvityTitle();
         } else {
             Log.e("Intent", "Invalid call!");
+        }
+    }
+
+    @Override
+    // Method to configure Back/Up-Button
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
