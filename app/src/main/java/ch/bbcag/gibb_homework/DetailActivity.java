44622b2e58519b538fa9899b5356e0cd6e04c432
Yusuf Khasbulatov <khashashin@gmail.com>
2021-01-30 01:58:53 +0100

    fixed back button on detail view

--- app/src/main/java/ch/bbcag/gibb_homework/DetailActivity.java
+++ app/src/main/java/ch/bbcag/gibb_homework/DetailActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
 import ch.bbcag.gibb_homework.model.Task;
@@ -39,5 +40,17 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    // Method to configure Back/Up-Button
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
