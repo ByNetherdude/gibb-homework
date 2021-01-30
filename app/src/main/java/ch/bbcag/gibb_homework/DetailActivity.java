b13f33c5fe13ee17a9af0c6bee7d361c9fa1eae0
Yusuf Khasbulatov <khashashin@gmail.com>
2021-01-31 00:23:14 +0100

    removed fab from detail activity

--- app/src/main/java/ch/bbcag/gibb_homework/DetailActivity.java
+++ app/src/main/java/ch/bbcag/gibb_homework/DetailActivity.java
@@ -27,14 +27,6 @@ public class DetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Back-Button
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
