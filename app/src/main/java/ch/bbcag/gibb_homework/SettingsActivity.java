f218d81c17b873db20cf4c2af2fc73bab26df486
khashashin <khashashin@users.noreply.github.com>
2021-02-01 11:48:03 +0100

    use custom list adapter for settings list

--- app/src/main/java/ch/bbcag/gibb_homework/SettingsActivity.java
+++ app/src/main/java/ch/bbcag/gibb_homework/SettingsActivity.java
@@ -3,6 +3,13 @@ package ch.bbcag.gibb_homework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import ch.bbcag.gibb_homework.components.modul.list.ModulListAdapter;
+import ch.bbcag.gibb_homework.dal.ModuleDAO;
+import ch.bbcag.gibb_homework.model.Modul;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +17,16 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ModuleDAO modulDAO = new ModuleDAO(this);
+        ArrayList<Modul> moduls = modulDAO.all();
+        ModulListAdapter modulListAdapter = new ModulListAdapter(moduls, this);
+        modulListAdapter.addAll(moduls);
+        ListView taskList = findViewById(R.id.task_list);
+        taskList.setAdapter(modulListAdapter);
     }
 }
