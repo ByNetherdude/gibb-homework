abe2638c5730d77c6277f23b11b8a3956e600b80
khashashin <khashashin@users.noreply.github.com>
2021-02-01 11:03:24 +0100

    Added is done attribute in to task entry

--- app/src/main/java/ch/bbcag/gibb_homework/database/TaskEntry.java
+++ app/src/main/java/ch/bbcag/gibb_homework/database/TaskEntry.java
@@ -8,4 +8,5 @@ public class TaskEntry {
     public static final String COLUMN_DUE_DATE = "due_date";
     public static final String COLUMN_IMAGE_FILE = "image_file";
     public static final String COLUMN_MODULE_ID = "module_id";
+    public static final String COLUMN_IS_DONE = "is_done";
 }
